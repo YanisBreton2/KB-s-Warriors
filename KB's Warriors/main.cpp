cef18c87b277ec65174a074bb44304c6b70efccf
YanisBreton2 <yanis.breton@hotmail.fr>
2015-11-30 20:00:11 +0100

    init project

---
+++ KB's Warriors/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <SFML\Graphics.hpp>
+
+
+int	main()
+{
+	sf::RenderWindow window(sf::VideoMode(1400, 800), "Runner");
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+	return (0);
+}
